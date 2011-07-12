0dff2918852911412213d4e0fff51d4142ee6fbb
RussellYermal <RussellYermal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-12 10:06:18 +0000

    Ultimate Follow Me's Official Debut on WordPress SVN
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-follow-me/trunk@408679 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Ultimate Follow Me ===
+Contributors: RussellYermal
+Donate link: http://freeblogfactory.com/
+Tags: social, follow, widgets
+Requires at least: 3.0
+Tested up to: 3.2
+Stable tag: trunk
+
+We were tired of confusing and bulky "follow me" plugins for wordpress so we decided to make our own. 
+
+== Description ==
+
+Ultimate Follow Me is a Simple yet Valuable Plugin.
+
+Easily link to your Facebook, Twitter, LinkedIn, and Youtube profiles in this widget so that your readers can find you across the web.
+
+As an added bonus, include your RSS feed and a "Contact Me" link where users can send you email or be redirected to any page (preferably a "Contact Me" page on your site).
+
+This plugin includes 3 different styles of icons for you to choose from.
+
+== Installation ==
+
+1. Upload `ultimate-follow-me.php` and the "style1", "style2", "style3" directories to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to Appearance->Widgets, drag the widget to any sidebar, and choose your options
+4. Profit
+
+== Frequently Asked Questions ==
+
+= How do I change the settings? =
+
+Go to Appearance->Widgets and change the settings
+
+= I pasted in http://facebook.com/some_random_username and it doesn't work, what gives? =
+
+Enter your facebook username (ie, facebook.com/username) or profile id (ie, facebook.com/profile.php?id=your_profile_id) and the widget will automagically figure out the rest.
+
+== Screenshots ==
+
+1. Ultimate Follow Me Widget Settings
+2. Ultimate Follow Me Widget In Action
+
+== Changelog ==
+
+= 1.2 =
+* Started counting versions.
+* Removed Google Buzz (RIP)
+
+== Upgrade Notice ==
+
+= 1.2 =
+Official release. Now hosted on WordPress SVN
+
+== Shameless Plug ==
+
+We built this for our thousands of users at [Free Blog Factory](http://FreeBlogFactory.com/ "Building better blogs"). If you want a reliable WordPress host that actually helps you use WordPress, check us out!
---
+++ ultimate-follow-me.php
@@ -0,0 +1,251 @@
+<?php
+/*
+Plugin Name: Ultimate Follow Me Plugin by Free Blog Factory
+Plugin URI: http://FreeBlogFactory.com
+Description: We were tired of confusing and bulky "follow me" plugins for wordpress so we decided to make our own. Showcase any or all of your facebook, twitter, linkedin, youtube, and buzz profiles with your choice of button design.
+Version: 1.1
+Author: Free Blog Factory
+Author URI: http://FreeBlogFactory.com
+License: GPL2
+*/
+
+/*  Copyright 2010  Russell Yermal (email : russell@socialspin.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+//error_reporting(E_ALL);
+
+add_action("widgets_init", array('ultimate_follow_me', 'register'));
+
+	$path_to_styles = WP_PLUGIN_URL . '/ultimate-follow-me';
+	$preview_image = 'preview.png';
+	$twitter_image = 'twitter.png';
+	$facebook_image = 'facebook.png';	
+	$youtube_image = 'youtube.png';	
+	$linkedin_image = 'linkedin.png';	
+	$email_image = 'email.png';	
+	$rss_image = 'rss.png';
+	
+	function create_facebook_tag($id, $style_id){
+		if(strlen($id) < 1)
+			return;
+			
+		$url = "http://facebook.com";
+		if(is_numeric($id))
+			$url .= "/profile.php?id=$id";
+		else
+			$url .= "/$id";
+			
+		$img = 	'<img src="'.get_facebook_image($style_id).'" border="0" style="margin:3px;"/>';
+			
+		return '<a href="'.$url.'" target="_blank">'.$img.'</a>';
+	}	
+	function create_twitter_tag($id, $style_id){
+		if(strlen($id) < 1)
+			return;
+			
+		$url = "http://twitter.com/$id";
+			
+		$img = 	'<img src="'.get_twitter_image($style_id).'" border="0" style="margin:3px;"/>';
+			
+		return '<a href="'.$url.'" target="_blank">'.$img.'</a>';
+	}
+	function create_youtube_tag($id, $style_id){
+		if(strlen($id) < 1)
+			return;
+			
+		$url = "http://youtube.com/$id";
+			
+		$img = 	'<img src="'.get_youtube_image($style_id).'" border="0" style="margin:3px;"/>';
+			
+		return '<a href="'.$url.'" target="_blank">'.$img.'</a>';
+	}	
+	function create_linkedin_tag($id, $style_id){
+		if(strlen($id) < 1)
+			return;
+			
+		$url = "http://linkedin.com/in/$id";
+			
+		$img = 	'<img src="'.get_linkedin_image($style_id).'" border="0" style="margin:3px;"/>';
+			
+		return '<a href="'.$url.'" target="_blank">'.$img.'</a>';
+	}		
+	function create_rss_tag($display, $style_id){
+		if($display != 'on')
+			return;
+			
+		$site_url = wp_guess_url();
+			
+		if($site_url[strlen($site_url)-1] != '/')
+			$site_url .= '/';	
+			
+		$url = $site_url."feed";
+			
+		$img = 	'<img src="'.get_rss_image($style_id).'" border="0" style="margin:3px;"/>';
+			
+		return '<a href="'.$url.'" target="_blank">'.$img.'</a>';
+	}		
+	function create_email_tag($address, $style_id){
+		if(strlen($address) < 1)
+			return;
+			
+		if(preg_match("/^[0-9a-z]([-_.]?[0-9a-z])*@[0-9a-z]([-.]?[0-9a-z])*\\.[a-z]{2,}\$/i",$address))
+			$url = "mailto:$address";
+		else
+			$url = $address;
+			
+		$img = 	'<img src="'.get_email_image($style_id).'" border="0" style="margin:3px;"/>';
+			
+		return '<a href="'.$url.'">'.$img.'</a>';
+	}		
+	
+	function get_preview_image($style_id){
+		global $path_to_styles;
+		global $preview_image;
+		return $path_to_styles.'/style'.$style_id.'/'.$preview_image;
+	}
+	function get_facebook_image($style_id){
+		global $path_to_styles;
+		global $facebook_image;		
+		return $path_to_styles.'/style'.$style_id.'/'.$facebook_image;
+	}	
+	function get_twitter_image($style_id){
+		global $path_to_styles;
+		global $twitter_image;		
+		return $path_to_styles.'/style'.$style_id.'/'.$twitter_image;
+	}		
+	function get_youtube_image($style_id){
+		global $path_to_styles;
+		global $youtube_image;		
+		return $path_to_styles.'/style'.$style_id.'/'.$youtube_image;
+	}		
+	function get_linkedin_image($style_id){
+		global $path_to_styles;
+		global $linkedin_image;		
+		return $path_to_styles.'/style'.$style_id.'/'.$linkedin_image;
+	}				
+	function get_email_image($style_id){
+		global $path_to_styles;
+		global $email_image;		
+		return $path_to_styles.'/style'.$style_id.'/'.$email_image;
+	}	
+	function get_rss_image($style_id){
+		global $path_to_styles;
+		global $rss_image;		
+		return $path_to_styles.'/style'.$style_id.'/'.$rss_image;
+	}		 
+
+	function update_options($data){
+		if(isset($_POST['uf_display_rss']))	
+			$data['uf_display_rss'] = $_POST['uf_display_rss'];			
+		if(isset($_POST['uf_fb_id']))
+			$data['uf_fb_id'] = $_POST['uf_fb_id'];
+		if(isset($_POST['uf_tw_id']))
+			$data['uf_tw_id'] = $_POST['uf_tw_id'];
+		if(isset($_POST['uf_yt_id']))
+			$data['uf_yt_id'] = $_POST['uf_yt_id'];
+		if(isset($_POST['uf_li_id']))
+			$data['uf_li_id'] = $_POST['uf_li_id'];
+		if(isset($_POST['uf_email_address']))
+			$data['uf_email_address'] = $_POST['uf_email_address'];
+			
+		if(isset($_POST['uf_style']))
+			$data['uf_style'] = $_POST['uf_style'];	
+				
+
+		update_option('ultimate_follow_me', $data);	
+	}
+
+class ultimate_follow_me { 	
+	
+  function control(){
+	  $data = get_option('ultimate_follow_me');
+	  ?>
+      	<table>
+        	<tr>
+            	<td align="center"><b>Name</b></td>
+            	<td align="center"><b>Input</b></td>
+            </tr>
+            <tr>
+	  			
+                <td align="right"><label>Facebook username or profile ID <small>(http://facebook.com/<u>username</u><br/>or http://facebook.com/profile.php?id=<u>profile_id</u>)</small>:</label></td>
+                <td valign="bottom"><input name="uf_fb_id" type="text" value="<?php echo $data['uf_fb_id']; ?>" /></td>
+            </tr>
+            <tr>
+                <td align="right"><label>Twitter username <small>(http://www.twitter.com/<u>username</u>)</small>:</label></td>
+                <td><input name="uf_tw_id" type="text" value="<?php echo $data['uf_tw_id']; ?>" /></td>
+            </tr>
+            <tr>
+	  			
+                <td align="right"><label>Youtube username <small>(http://www.youtube.com/<u>username</u>)</small>:</label></td>
+                <td><input name="uf_yt_id" type="text" value="<?php echo $data['uf_yt_id']; ?>" /></td>
+            </tr>
+<tr>
+	  			
+                <td align="right"><label>Linkedin username <small>(http://www.linkedin.com/in/<u>username</u>)</small>:</label></td>
+                <td><input name="uf_li_id" type="text" value="<?php echo $data['uf_li_id']; ?>" /></td>
+            </tr>              
+<tr>
+                <td align="right"><label>Email address or URL to contact us page:</label></td>
+                <td><input name="uf_email_address" type="text" value="<?php echo $data['uf_email_address']; ?>" /></td>
+            </tr> 
+            <tr>
+                <td align="right"><label>Display rss feed?</label></td>
+                <td><input name="uf_display_rss" type="checkbox" <?php if($data['uf_display_rss']=='on') echo 'checked'; ?>/></td>
+            </tr>              
+<tr>
+            	<td colspan="3" align="center">
+                	<b>Display style</b>
+                </td>
+            </tr>
+            <tr>
+            	<td colspan="3" align="center"><input type="radio" name="uf_style" value="1" <?php if ($data['uf_style'] == 1) echo 'checked'; ?>/><img src="<?=get_preview_image(1); ?>">
+                </td>
+            </tr>
+            <tr>            
+				<td colspan="3" align="center"><input type="radio" name="uf_style" value="2" <?php if ($data['uf_style'] == 2) echo 'checked'; ?>/><img src="<?=get_preview_image(2); ?>"> 
+                </td>
+            </tr> 
+            <tr>          
+				<td colspan="3" align="center"><input type="radio" name="uf_style" value="3" <?php if ($data['uf_style'] == 3) echo 'checked'; ?>/><img src="<?=get_preview_image(3); ?>">
+               	</td>
+            </tr>            
+       </table>
+	  <?php
+		
+		update_options($data);
+	}	
+	
+  function widget($args){
+    echo $args['before_widget'];
+    echo $args['before_title'] . 'Follow me on the web!' . $args['after_title'];
+    	  $data = get_option('ultimate_follow_me');
+    	  echo '<div id="ultimate-follow-me">';
+		  echo create_facebook_tag($data['uf_fb_id'], $data['uf_style']);
+		  echo create_twitter_tag($data['uf_tw_id'], $data['uf_style']);
+		  echo create_youtube_tag($data['uf_yt_id'], $data['uf_style']);
+		  echo create_linkedin_tag($data['uf_li_id'], $data['uf_style']);
+		  echo create_email_tag($data['uf_email_address'], $data['uf_style']);
+		  echo create_rss_tag($data['uf_display_rss'], $data['uf_style']);
+		  echo '</div>';
+    echo $args['after_widget'];
+  }
+  
+  function register(){
+
+    register_sidebar_widget('Ultimate Follow Me', array('ultimate_follow_me', 'widget'));
+    register_widget_control('Ultimate Follow Me', array('ultimate_follow_me', 'control'), 600, 400);
+  }
+}
+?>
