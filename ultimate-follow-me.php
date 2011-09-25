d42a3658674c20e0f3ffd9015f6de932affa1b2d
RussellYermal <RussellYermal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-25 18:29:50 +0000

    Patched FancyBox conflict
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-follow-me/tags/1.3.2@443314 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ultimate-follow-me.php
+++ ultimate-follow-me.php
@@ -3,7 +3,7 @@
 Plugin Name: Ultimate Follow Me Plugin by Free Blog Factory
 Plugin URI: http://FreeBlogFactory.com
 Description: We were tired of confusing and bulky "follow me" plugins for wordpress so we decided to make our own. Showcase any or all of your facebook, twitter, linkedin, youtube, and buzz profiles with your choice of button design.
-Version: 1.2
+Version: 1.3.2
 Author: Free Blog Factory
 Author URI: http://FreeBlogFactory.com
 License: GPL2
@@ -31,6 +31,7 @@ License: GPL2
 	$path_to_styles = WP_PLUGIN_URL . '/ultimate-follow-me';
 	$preview_image = 'preview.png';
 	$twitter_image = 'twitter.png';
+	$plus_image = 'plus.png';
 	$facebook_image = 'facebook.png';	
 	$youtube_image = 'youtube.png';	
 	$linkedin_image = 'linkedin.png';	
@@ -61,13 +62,23 @@ License: GPL2
 			
 		return '<a href="'.$url.'" target="_blank">'.$img.'</a>';
 	}
+	function create_plus_tag($id, $style_id){
+		if(strlen($id) < 1)
+			return;
+			
+		$url = "https://plus.google.com/u/1/$id/posts";
+			
+		$img = 	'<img src="'.get_plus_image($style_id).'" border="0" style="margin:3px;"/>';
+			
+		return '<a href="'.$url.'" target="_blank">'.$img.'</a>';
+	}
 	function create_youtube_tag($id, $style_id){
 		if(strlen($id) < 1)
 			return;
 			
 		$url = "http://youtube.com/$id";
 			
-		$img = 	'<img src="'.get_youtube_image($style_id).'" border="0" style="margin:3px;"/>';
+		$img = 	'<img src="'.get_youtube_image($style_id).'" border="0" class="nofancybox" style="margin:3px;"/>';
 			
 		return '<a href="'.$url.'" target="_blank">'.$img.'</a>';
 	}	
@@ -85,12 +96,7 @@ License: GPL2
 		if($display != 'on')
 			return;
 			
-		$site_url = wp_guess_url();
-			
-		if($site_url[strlen($site_url)-1] != '/')
-			$site_url .= '/';	
-			
-		$url = $site_url."feed";
+		$url = get_bloginfo('rss2_url');;
 			
 		$img = 	'<img src="'.get_rss_image($style_id).'" border="0" style="margin:3px;"/>';
 			
@@ -124,7 +130,12 @@ License: GPL2
 		global $path_to_styles;
 		global $twitter_image;		
 		return $path_to_styles.'/style'.$style_id.'/'.$twitter_image;
-	}		
+	}
+	function get_plus_image($style_id){
+		global $path_to_styles;
+		global $plus_image;		
+		return $path_to_styles.'/style'.$style_id.'/'.$plus_image;
+	}			
 	function get_youtube_image($style_id){
 		global $path_to_styles;
 		global $youtube_image;		
@@ -147,12 +158,21 @@ License: GPL2
 	}		 
 
 	function update_options($data){
-		if(isset($_POST['uf_display_rss']))	
-			$data['uf_display_rss'] = $_POST['uf_display_rss'];			
+	
+		if(empty($_POST))
+			return;
+	
+		if($_POST['uf_display_rss']=='on')	
+			$data['uf_display_rss'] = 'on';
+		else
+			$data['uf_display_rss'] = 'off';
+					
 		if(isset($_POST['uf_fb_id']))
 			$data['uf_fb_id'] = $_POST['uf_fb_id'];
 		if(isset($_POST['uf_tw_id']))
 			$data['uf_tw_id'] = $_POST['uf_tw_id'];
+		if(isset($_POST['uf_gp_id']))
+			$data['uf_gp_id'] = $_POST['uf_gp_id'];
 		if(isset($_POST['uf_yt_id']))
 			$data['uf_yt_id'] = $_POST['uf_yt_id'];
 		if(isset($_POST['uf_li_id']))
@@ -163,6 +183,8 @@ License: GPL2
 		if(isset($_POST['uf_style']))
 			$data['uf_style'] = $_POST['uf_style'];	
 				
+		if(isset($_POST['uf_title']))
+			$data['uf_title'] = $_POST['uf_title'];			
 
 		update_option('ultimate_follow_me', $data);	
 	}
@@ -171,38 +193,55 @@ class ultimate_follow_me {
 	
   function control(){
 	  $data = get_option('ultimate_follow_me');
+	  
+	  if($data['uf_title']!='')
+	  	$title = $data['uf_title'];
+	  else
+	  	$title = "Follow Me On The Web!";
+	  	
+	  $check_rss='';	
+	  	
+	  if($data['uf_display_rss']=='on')
+	  	$check_rss='checked';
+
 	  ?>
       	<table>
-        	<tr>
-            	<td align="center"><b>Name</b></td>
-            	<td align="center"><b>Input</b></td>
+            <tr>
+	  			
+                <td align="right"><label>Title:</label></td>
+                <td valign="bottom"><input name="uf_title" type="text" value="<?=$title?>" /></td>
             </tr>
             <tr>
 	  			
                 <td align="right"><label>Facebook username or profile ID <small>(http://facebook.com/<u>username</u><br/>or http://facebook.com/profile.php?id=<u>profile_id</u>)</small>:</label></td>
-                <td valign="bottom"><input name="uf_fb_id" type="text" value="<?php echo $data['uf_fb_id']; ?>" /></td>
+                <td valign="bottom"><input name="uf_fb_id" type="text" value="<?=$data['uf_fb_id']?>" /></td>
             </tr>
             <tr>
                 <td align="right"><label>Twitter username <small>(http://www.twitter.com/<u>username</u>)</small>:</label></td>
-                <td><input name="uf_tw_id" type="text" value="<?php echo $data['uf_tw_id']; ?>" /></td>
+                <td><input name="uf_tw_id" type="text" value="<?=$data['uf_tw_id']?>" /></td>
             </tr>
+            <tr>
+	  			
+                <td align="right"><label>Linkedin username <small>(http://www.linkedin.com/in/<u>username</u>)</small>:</label></td>
+                <td><input name="uf_li_id" type="text" value="<?=$data['uf_li_id']?>" /></td>
+            </tr> 
+            <tr>
+	  			
+                <td align="right"><label>Google Plus user ID <small>(https://plus.google.com/u/1/<u>user ID</u>)</small>:</label></td>
+                <td><input name="uf_gp_id" type="text" value="<?=$data['uf_gp_id']?>" /></td>
+            </tr> 
             <tr>
 	  			
                 <td align="right"><label>Youtube username <small>(http://www.youtube.com/<u>username</u>)</small>:</label></td>
-                <td><input name="uf_yt_id" type="text" value="<?php echo $data['uf_yt_id']; ?>" /></td>
-            </tr>
-<tr>
-	  			
-                <td align="right"><label>Linkedin username <small>(http://www.linkedin.com/in/<u>username</u>)</small>:</label></td>
-                <td><input name="uf_li_id" type="text" value="<?php echo $data['uf_li_id']; ?>" /></td>
-            </tr>              
+                <td><input name="uf_yt_id" type="text" value="<?=$data['uf_yt_id']?>" /></td>
+            </tr>       
 <tr>
                 <td align="right"><label>Email address or URL to contact us page:</label></td>
-                <td><input name="uf_email_address" type="text" value="<?php echo $data['uf_email_address']; ?>" /></td>
+                <td><input name="uf_email_address" type="text" value="<?=$data['uf_email_address']?>" /></td>
             </tr> 
             <tr>
                 <td align="right"><label>Display rss feed?</label></td>
-                <td><input name="uf_display_rss" type="checkbox" <?php if($data['uf_display_rss']=='on') echo 'checked'; ?>/></td>
+                <td><input name="uf_display_rss" type="checkbox" <?=$check_rss?>/></td>
             </tr>              
 <tr>
             	<td colspan="3" align="center">
@@ -228,14 +267,23 @@ class ultimate_follow_me {
 	}	
 	
   function widget($args){
+  
+    $data = get_option('ultimate_follow_me');
+  
+  	if($data['uf_title']!='')
+	  	$title = $data['uf_title'];
+	  else
+	  	$title = "Follow Me On The Web!";
+  
     echo $args['before_widget'];
-    echo $args['before_title'] . 'Follow me on the web!' . $args['after_title'];
-    	  $data = get_option('ultimate_follow_me');
+    echo $args['before_title'] . $title . $args['after_title'];
+    	  
     	  echo '<div id="ultimate-follow-me">';
 		  echo create_facebook_tag($data['uf_fb_id'], $data['uf_style']);
 		  echo create_twitter_tag($data['uf_tw_id'], $data['uf_style']);
-		  echo create_youtube_tag($data['uf_yt_id'], $data['uf_style']);
 		  echo create_linkedin_tag($data['uf_li_id'], $data['uf_style']);
+		  echo create_plus_tag($data['uf_gp_id'], $data['uf_style']);
+		  echo create_youtube_tag($data['uf_yt_id'], $data['uf_style']);
 		  echo create_email_tag($data['uf_email_address'], $data['uf_style']);
 		  echo create_rss_tag($data['uf_display_rss'], $data['uf_style']);
 		  echo '</div>';
